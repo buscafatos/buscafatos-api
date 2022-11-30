3e4c93b15f368ff4899cd10fd48a61471ca180d3
fcavalcantirj <felipe.cavalcanti.rj@gmail.com>
2022-11-30 18:09:45 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,120 @@
+require('dotenv').config()
+
+const puppeteer = require('puppeteer');
+const parse = require('url-parse');
+const { transform } = require("node-json-transform");
+const express = require('express')
+const app = express()
+const port = 8000
+
+const SEARCH_ENGINE_ID = process.env.SEARCH_ENGINE_ID;
+const API_KEY = process.env.API_KEY;
+
+let baseMap = {
+  item: {
+    totalResults: "queries.request.0.totalResults",
+    searchTerms: "queries.request.0.searchTerms",
+    count: "queries.request.0.count",
+    startIndex: "queries.request.0.startIndex",
+    items: "items"
+  },
+  operate: [
+    {
+      run: function(ary) { 
+        return transform(ary, nestedMap);
+      }, 
+      on: "items"
+    }
+  ]
+};
+
+let nestedMap = {
+  "item" : {
+    "title": "title",
+    "source": "displayLink",
+    "htmlTitle": "htmlTitle",
+    "link": "link",
+    "snippet": "snippet",
+    "htmlSnippet": "htmlSnippet",
+    "thumbnail": "pagemap.cse_thumbnail.0.src"
+  }
+};
+
+
+async function asyncHandler(requestUrl, query) {
+
+    console.log('searchTerm = [%s] --- requestUrl = [%s]', query, requestUrl);
+
+    const queryObject = parse(requestUrl, true).query;
+
+    // console.log('last search term [%s]', query);
+
+    // https://developers.google.com/custom-search/v1/overview
+    // https://developers.google.com/custom-search/v1/reference/rest/v1/cse/list#request
+
+    let _url = new URL('https://www.googleapis.com/customsearch/v1/siterestrict');
+    _url.searchParams.set('num', '10');
+    _url.searchParams.set('safe', 'active');
+    _url.searchParams.set('q', query);
+    _url.searchParams.set('key', API_KEY);
+    _url.searchParams.set('cx', SEARCH_ENGINE_ID);
+
+    console.log(_url);
+
+    // pagination
+    if (queryObject.st) {
+      let start = parseInt(queryObject.st);
+      if (Number.isInteger(start))
+      _url.searchParams.set('start', start);
+    }
+
+    // count
+    if (queryObject.count) {
+      let count = parseInt(queryObject.count);
+      if (Number.isInteger(count))
+      _url.searchParams.set('num', count);
+    }
+
+    // sort
+    if (queryObject.sort) {
+      _url.searchParams.set('sort', queryObject.sort);
+    }      
+
+
+    const resp = await fetch(_url, {
+      headers: {
+        accept: "application/json",
+      },
+    });
+
+    // raw json from google
+    if (queryObject.raw && '1' == queryObject.raw) {
+
+      return resp.body;
+
+    } else {
+      // simpler transformed json
+
+      let json = await resp.json();
+      // console.log(json);
+      let _result = transform(json, baseMap);
+      // console.log(_result);
+      return _result;
+
+    }
+}
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+app.get('/v1/search/:query', async (req, res) => {
+  let fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+  let _json = await asyncHandler(fullUrl, req.params.query);
+  res.json(_json);
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
